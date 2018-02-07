2592244f35f818ead96d563b33c751fcf588fa71
Unknown <sbsmith@beckman.com>
2018-02-07 15:55:39 -0500

    RecipeList working with XML import/export

--- RecipeList/ViewModel/Presenter.cs
+++ RecipeList/ViewModel/Presenter.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 using Model;
+using System.Xml;
+using System.IO;
 
 namespace ViewModel
 {
     public class Presenter : ObservableObject
     {
         private readonly TextConverter _textConverter = new TextConverter(x => x.ToUpper());
-        private readonly ObservableCollection<Ingredient> _ingredients = new ObservableCollection<Ingredient>();
+        private ObservableCollection<Ingredient> _ingredients = new ObservableCollection<Ingredient>();
         private string _name;
         private string _quantity;
         private string _unit;
+        private readonly string RECIPE_FILE_NAME = "Recipe.xml";
 
         public string Name
         {
@@ -47,6 +51,7 @@ namespace ViewModel
             }
         }
 
+        //Not Sure what this does
         public IEnumerable<Ingredient> History
         {
             get { return _ingredients; }
@@ -57,15 +62,25 @@ namespace ViewModel
             get { return new DelegateCommand(AddItem); }
         }
 
+        public ICommand SaveRecipeCommand
+        {
+            get { return new DelegateCommand(WriteXMLString); }
+        }
+
+        public ICommand LoadRecipeCommand
+        {
+            get { return new DelegateCommand(LoadXMLString); }
+        }
+
         private void AddItem()
         {
-            bool AllPopulated = string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(Name);
+            bool AllPopulated = string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(Quantity) || string.IsNullOrWhiteSpace(Unit);
             if (AllPopulated)
             {
                 return;
             }
 
-            AddToHistory(new Ingredient(Name, Quantity, Unit));
+            _ingredients.Add(new Ingredient(Name, Quantity, Unit));
             Name = string.Empty;
             Quantity = string.Empty;
             Unit = string.Empty;
@@ -76,6 +91,72 @@ namespace ViewModel
             if (!_ingredients.Contains(item))
             {
                 _ingredients.Insert(0, item);
+            }
+        }
+
+        public void WriteXMLString()
+        {
+            File.WriteAllText(RECIPE_FILE_NAME, ToXMLString());
+        }
+
+        public void LoadXMLString()
+        {
+            CreateIngredientListFromXMLString(File.ReadAllText(RECIPE_FILE_NAME));
+        }
+
+        public string ToXMLString()
+        {
+            XmlDocument ingredientDoc = new XmlDocument();
+
+            XmlNode ingredients = ingredientDoc.CreateElement("Ingredients");
+            ingredients = ingredientDoc.AppendChild(ingredients);
+
+            foreach (Ingredient ingredient in _ingredients)
+            {
+                XmlNode ingredientNode = ingredientDoc.CreateElement("Ingredient");
+
+                XmlAttribute nameAttribute = ingredientDoc.CreateAttribute("IngredientName");
+                nameAttribute.Value = ingredient.Name;
+                ingredientNode.Attributes.Append(nameAttribute);
+
+                XmlAttribute quantityAttribute = ingredientDoc.CreateAttribute("Quantity");
+                quantityAttribute.Value = ingredient.Quantity;
+                ingredientNode.Attributes.Append(quantityAttribute);
+
+                XmlAttribute unitAttribute = ingredientDoc.CreateAttribute("Unit");
+                unitAttribute.Value = ingredient.Unit;
+                ingredientNode.Attributes.Append(unitAttribute);
+
+                ingredients.AppendChild(ingredientNode);
+            }
+
+            return ingredientDoc.InnerXml;
+        }
+
+        public void CreateIngredientListFromXMLString(string xmlIngredientList)
+        {
+            try
+            {
+                _ingredients.Clear();
+
+                XmlDocument ingredientDoc = new XmlDocument();
+
+                ingredientDoc.LoadXml(xmlIngredientList);
+
+                XmlNodeList listOfIngredients = ingredientDoc.SelectNodes("/Ingredients/Ingredient");
+
+                foreach (XmlNode ingredient in listOfIngredients)
+                {
+                    string name = ingredient.Attributes["IngredientName"].Value;
+                    string quantity = ingredient.Attributes["Quantity"].Value;
+                    string unit = ingredient.Attributes["Unit"].Value;
+
+                    _ingredients.Add(new Ingredient(name, quantity, unit));
+                }
+            }
+            catch
+            {
+                //Do nothing
             }
         }
     }
